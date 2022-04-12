b6756c13cd1ac33e787d7a54692db301b1815661
ewoifuoi <hamimelons@126.com>
2022-04-12 22:40:07 +0800

    update

--- main.js
+++ main.js
@@ -6,12 +6,23 @@ var auto_check = require('func.auto_check');
 var roleKeeper = require('role.towerKeeper');
 
 module.exports.loop = function () {
-    var emergency = false;
+    
+    const creepsName = Object.keys(Game.creeps)// 清理 死去 creep 内存
+    Object.keys(Memory.creeps).forEach(name => {
+        if (!creepsName.includes(name)) {
+            delete Memory.creeps[name]
+        }
+    });
+    
+    
+    
+    
+    var emergency = false;  // 紧急模式
     var tower = Game.getObjectById('6254e60c2740d17db2fe533f'); // 一号炮塔
     if(tower) {
         var attacked = false;
         var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if(closestHostile) {
+        if(closestHostile) {                // 炮塔攻击
             tower.attack(closestHostile);
             emergency = true;
             attacked = true;
@@ -21,20 +32,20 @@ module.exports.loop = function () {
         }
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (structure) => {
-                return (structure.hits < structure.hitsMax) && (structure.hits < 10000);
+                return (structure.hits < structure.hitsMax) && (structure.hits < 10000);    // 炮塔给建筑 回血 (墙上限 10000)
             }
         });
-        if(tower.store[RESOURCE_ENERGY] > 800) {
+        if(tower.store[RESOURCE_ENERGY] > 800) {            // 炮塔 存量 > 800 , 启动回血模式
             if(closestDamagedStructure && !attacked) {
                 tower.repair(closestDamagedStructure);
             }
         }
     }
     
-    auto_check.run();
+    auto_check.run();       // 自动 补充 creep
     
     
-    var targets = Game.spawns['716'].room.find(FIND_STRUCTURES, {
+    var targets = Game.spawns['716'].room.find(FIND_STRUCTURES, {           // 能量 需求 单位
         filter: (structure) => {
             return (structure.structureType == STRUCTURE_EXTENSION ||
             structure.structureType == STRUCTURE_SPAWN) && 
@@ -43,9 +54,9 @@ module.exports.loop = function () {
     });
 
    
-    var sources = Game.spawns['716'].room.find(FIND_SOURCES);
+    var sources = Game.spawns['716'].room.find(FIND_SOURCES);   // 资源 单位
 
-    for(var name in Game.creeps) {
+    for(var name in Game.creeps) {          // 遍历 每个 creep , 根据 其中内存 role 字段 进行分工
         var creep = Game.creeps[name];
         if(creep.memory.role == 'farmer') {
             if(emergency == true) {
@@ -72,8 +83,8 @@ module.exports.loop = function () {
             else if(targets.length > 0) {
                 roleHarvester.run(creep, sources[1], targets);
             }
-            // else roleUpgrader2.run(creep);
-            else roleBuilder.run(creep);
+            else roleUpgrader2.run(creep);
+            // else roleBuilder.run(creep);
         }
         if(creep.memory.role == 'keeper') {
             roleKeeper.run(creep, tower, sources[0]);
